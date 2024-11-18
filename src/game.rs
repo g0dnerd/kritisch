00a839141b2f6cf816d50f6142bf71fcd1377548
Paul Kukowski <kukowskipaul@gmail.com>
2024-11-18 20:49:45 +0100

    Added FEN support

--- src/game.rs
+++ src/game.rs
@@ -1,5 +1,6 @@
 use crate::{
-    bitboard::Bitboard, CastlingRights, Color, Move, Piece, Square, PIECE_REPR_B, PIECE_REPR_W,
+    bitboard::Bitboard, CastlingRights, Color, File, Move, Piece, Square, PIECE_REPR_B,
+    PIECE_REPR_W,
 };
 use anyhow::Context;
 
@@ -22,6 +23,7 @@ impl std::fmt::Display for IllegalMoveError {
 
 impl std::error::Error for IllegalMoveError {}
 
+#[derive(Debug, PartialEq, Eq)]
 pub struct Game {
     pub color_bitboards: [Bitboard; 2],
     pub piece_bitboards: [Bitboard; 6],
@@ -29,7 +31,7 @@ pub struct Game {
     pub to_move: Color,
     pub castling_rights: u8,
 
-    pub en_passant: Option<Square>,
+    pub en_passant_square: Option<Square>,
     pub in_check: Option<Color>,
 
     pub halfmove_clock: usize,
@@ -111,7 +113,7 @@ impl std::default::Default for Game {
             piece_bitboards,
             to_move: Color::WHITE,
             castling_rights: CastlingRights::ALL_LEGAL,
-            en_passant: None,
+            en_passant_square: None,
             in_check: None,
             halfmove_clock: 0,
             fullmove_clock: 1,
@@ -123,9 +125,9 @@ impl std::fmt::Display for Game {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut board = String::new();
         board.push('\n');
-        for i in 0..64 {
-            let file = i % 8;
-            let rank = i / 8;
+        for s in 0..64 {
+            let file = s % 8;
+            let rank = s / 8;
             let square = Square::from_u8(64 - (rank * 8 + 8 - file));
             if self.color_bitboards[0].contains(square) {
                 for (piece_idx, piece_bb) in self.piece_bitboards.iter().enumerate() {
@@ -144,7 +146,7 @@ impl std::fmt::Display for Game {
             } else {
                 board.push_str(". ");
             }
-            if file == 7 {
+            if File::from_u8(file) == File::H {
                 board.push('\n');
             }
         }
@@ -153,6 +155,182 @@ impl std::fmt::Display for Game {
 }
 
 impl Game {
+    fn empty() -> Self {
+        let color_bitboards = [Bitboard::empty(); 2];
+        let piece_bitboards = [Bitboard::empty(); 6];
+
+        Self {
+            color_bitboards,
+            piece_bitboards,
+            to_move: Color::WHITE,
+            castling_rights: CastlingRights::ALL_LEGAL,
+            en_passant_square: None,
+            in_check: None,
+            halfmove_clock: 0,
+            fullmove_clock: 1,
+        }
+    }
+    /// Tries to parse the given FEN string into a position
+    pub fn from_fen(fen: &'static str) -> anyhow::Result<Self> {
+        let mut pos = Self::empty();
+        let mut square = Square::A8;
+
+        let mut index = 0;
+
+        for (i, c) in fen.chars().enumerate() {
+            if c == ' ' {
+                index = i + 1;
+                break;
+            }
+            if c.is_ascii_digit() {
+                let add = (c.to_digit(10).unwrap() as u8).clamp(1, 7);
+                square = square + add;
+                if square.get_file() == File::A {
+                    square = square - 1u8;
+                }
+            } else if c == '/' {
+                square = square - 15u8;
+            } else if PIECE_REPR_B.contains(&c) || PIECE_REPR_W.contains(&c) {
+                let piece = Piece::from_char(&c);
+                let color = if c.is_ascii_lowercase() {
+                    Color::BLACK
+                } else {
+                    Color::WHITE
+                };
+                pos.color_bitboards[color as usize] |= square;
+                pos.piece_bitboards[piece as usize] |= square;
+
+                if square.get_file() != File::H {
+                    square = square + 1u8;
+                }
+            } else {
+                anyhow::bail!("Unexpected character in FEN string");
+            }
+            index = i + 1;
+        }
+
+        if let Some(c) = fen.chars().nth(index) {
+            match c {
+                'w' => pos.to_move = Color::WHITE,
+                'b' => pos.to_move = Color::BLACK,
+                _ => anyhow::bail!("Expected color specification for player to move"),
+            }
+            index += 1
+        }
+
+        index += 1;
+
+        pos.castling_rights = CastlingRights::NO_LEGAL;
+
+        for c in fen[index..].chars() {
+            if c == ' ' {
+                index += 1;
+                break;
+            } else if c == '-' {
+                index += 2;
+                break;
+            }
+            match c {
+                'K' => pos.castling_rights |= CastlingRights::WHITE_KINGSIDE,
+                'Q' => pos.castling_rights |= CastlingRights::WHITE_QUEENSIDE,
+                'k' => pos.castling_rights |= CastlingRights::BLACK_KINGSIDE,
+                'q' => pos.castling_rights |= CastlingRights::BLACK_QUEENSIDE,
+                _ => anyhow::bail!("Unexpected character in castling rights section of FEN string"),
+            }
+            index += 1;
+        }
+
+        match fen.chars().nth(index) {
+            Some(c) => {
+                if c.is_ascii_lowercase() {
+                    match fen.chars().nth(index + 1) {
+                        Some(d) => {
+                            if d.is_ascii_digit() {
+                                match Square::from_parts(&c, &d) {
+                                    Ok(s) => pos.en_passant_square = Some(s),
+                                    Err(_) => anyhow::bail!(
+                                        "Couldn't parse en passant square in FEN string"
+                                    ),
+                                }
+                            }
+                        }
+                        None => anyhow::bail!(
+                            "Expected file while parsing en-passant square from FEN string"
+                        ),
+                    }
+                } else if c == '-' {
+                    index += 1;
+                }
+            }
+            None => anyhow::bail!("Incomplete FEN string - move counts missing"),
+        }
+
+        if fen.chars().nth(index) != Some(' ') {
+            anyhow::bail!(
+                "Error while parsing FEN string - expected whitespace after en passant square"
+            )
+        }
+        index += 1;
+
+        match fen.chars().nth(index) {
+            Some(c) => {
+                if c.is_ascii_digit() {
+                    let mut hmc = String::new();
+                    hmc.push(c);
+                    let mut peek = 1;
+                    loop {
+                        if let Some(n) = fen.chars().nth(index + peek) {
+                            if n == ' ' {
+                                break;
+                            }
+                            hmc.push(n);
+                            peek += 1;
+                        } else {
+                            anyhow::bail!("Incomplete FEN string - fullmove clock missing")
+                        }
+                    }
+                    pos.halfmove_clock = hmc
+                        .parse()
+                        .context("tried to cast FEN halfmove clock to usize")?;
+                    index += peek;
+                } else {
+                    anyhow::bail!("Expected digit in halfmove clock position in FEN string")
+                }
+            }
+            None => anyhow::bail!("Incomplete FEN string - halfmove clock missing"),
+        }
+
+        if fen.chars().nth(index) != Some(' ') {
+            anyhow::bail!(
+                "Error while parsing FEN string - expected whitespace after halfmove clock"
+            )
+        }
+        index += 1;
+
+        match fen.chars().nth(index) {
+            Some(c) => {
+                if c.is_ascii_digit() {
+                    let mut fmc = String::new();
+                    fmc.push(c);
+                    let mut peek = 1;
+                    while let Some(n) = fen.chars().nth(index + peek) {
+                        if n == ' ' {
+                            break;
+                        }
+                        fmc.push(n);
+                        peek += 1;
+                    }
+                    pos.fullmove_clock = fmc
+                        .parse()
+                        .context("tried to cast FEN fullmove clock to usize")?;
+                } else {
+                    anyhow::bail!("Expected digit in fullmove clock position in FEN string")
+                }
+            }
+            None => anyhow::bail!("Incomplete FEN string - fullmove clock missing"),
+        }
+        Ok(pos)
+    }
     /// Returns `Some(Piece)` if one of `self`'s piece bitboards
     /// contains `s` and `None` otherwise.
     pub fn type_at(&self, s: Square) -> Option<Piece> {
@@ -237,7 +415,8 @@ impl Game {
             self.fullmove_clock += 1;
         }
 
-        self.to_move = Color::from_u8(self.to_move as u8 ^ 1);
+        // Change which player's turn it is
+        self.to_move = self.to_move ^ 1;
 
         Ok(())
     }
@@ -273,13 +452,11 @@ impl Game {
         } else {
             match c {
                 Color::WHITE => {
-                    let target_square = Square::from_u8(m.end as u8 - 8);
-                    eprintln!("target square for removal: {}", target_square);
+                    let target_square = m.end - 8u8;
                     self.remove_piece(target_square, captured_piece)?;
                 }
                 Color::BLACK => {
-                    let target_square = Square::from_u8(m.end as u8 + 8);
-                    eprintln!("target square for removal: {}", target_square);
+                    let target_square = m.end + 8u8;
                     self.remove_piece(target_square, captured_piece)?;
                 }
             }
@@ -315,7 +492,7 @@ impl Game {
     }
 
     pub fn is_en_passant(&self, m: Move, captured_piece: Piece) -> bool {
-        self.en_passant == Some(m.end) && captured_piece == Piece::PAWN
+        self.en_passant_square == Some(m.end) && captured_piece == Piece::PAWN
     }
 
     fn castle(&mut self, _m: Move) -> anyhow::Result<()> {
